10ce1ea62a7699a1a9ee5b7d67db40ccaf556c51
Shakespeare <Shakespeare@asd.asd>
2024-09-24 11:51:58 +0000

    Deployed 57408da to master in public with MkDocs 1.6.1 and mike 2.1.3

--- public/master/execution-phase/dynamic_example.py
+++ public/master/execution-phase/dynamic_example.py
@@ -4,46 +4,10 @@ import time
 
 WORKER_ID = 0
 
-TEMPLATE = {
-    "plan": {
-        "name": "example",
-        "owner": "Cryton",
-        "dynamic": True,
-        "stages": []
-    }
-}
-
-STAGE = {
-    "name": "no delay stage {{ id }}",
-    "trigger_type": "delta",
-    "trigger_args": {
-        "seconds": 0
-    },
-    "steps": []
-}
-
-STEP = {
-    "name": "initial step",
-    "step_type": "worker/execute",
-    "is_init": True,
-    "arguments": {
-        "module": "command",
-        "module_arguments": {
-            "cmd": "whoami"
-        }
-    }
-}
-
-STEP_REUSABLE = {
-    "name": "reusable step {{ id }}",
-    "step_type": "worker/execute",
-    "arguments": {
-        "module": "command",
-        "module_arguments": {
-            "cmd": "{{ command }}"
-        }
-    }
-}
+TEMPLATE = {"name": "example", "dynamic": True, "stages": {}}
+STAGE = {"no-delay-stage-{{ id }}": {"steps": {}}}
+STEP = {"initial-step": {"is_init": True, "module": "command", "arguments": {"command": "whoami"}}}
+STEP_REUSABLE = {"reusable-step-{{ id }}": {"module": "command", "arguments": {"cmd": "{{ command }}"}}}
 
 
 def get_api_root():
@@ -63,48 +27,55 @@ if __name__ == "__main__":
 
     # 1. Create a template
     r_create_template = requests.post(f"{api_root}templates/", files={"file": yaml.dump(TEMPLATE)})
-    template_id = r_create_template.json()['id']
+    template_id = r_create_template.json()["id"]
     print(f"Template id: {template_id}")
 
     # 2. Create a Plan
-    r_create_plan = requests.post(f"{api_root}plans/", data={'template_id': template_id})
-    plan_id = r_create_plan.json()['id']
+    r_create_plan = requests.post(f"{api_root}plans/", data={"template_id": template_id})
+    plan_id = r_create_plan.json()["id"]
     print(f"Plan id: {plan_id}")
 
     # 3. Add a Stage
     stage_inventory = {"id": 1}
-    r_create_stage = requests.post(f"{api_root}stages/", data={'plan_id': plan_id},
-                                   files={"file": yaml.dump(STAGE), "inventory_file": yaml.dump(stage_inventory)})
-    stage_id = r_create_stage.json()['id']
+    r_create_stage = requests.post(
+        f"{api_root}stages/",
+        data={"plan_id": plan_id},
+        files={"file": yaml.dump(STAGE), "inventory_file": yaml.dump(stage_inventory)},
+    )
+    stage_id = r_create_stage.json()["id"]
     print(f"Stage id: {stage_id}")
 
     # 4. Add a Step
-    r_create_step = requests.post(f"{api_root}steps/", data={'stage_id': stage_id}, files={"file": yaml.dump(STEP)})
-    step_id = r_create_step.json()['id']
+    r_create_step = requests.post(f"{api_root}steps/", data={"stage_id": stage_id}, files={"file": yaml.dump(STEP)})
+    step_id = r_create_step.json()["id"]
     print(f"Step id: {step_id}")
 
     # 5. Create a new Run
-    r_create_run = requests.post(f"{api_root}runs/", data={'plan_id': plan_id, "worker_ids": [WORKER_ID]})
+    r_create_run = requests.post(f"{api_root}runs/", data={"plan_id": plan_id, "worker_ids": [WORKER_ID]})
     run_id = r_create_run.json()["id"]
     print(f"Run id: {run_id}")
 
     # 6. Execute the Run
-    r_execute_run = requests.post(f"{api_root}runs/{run_id}/execute/", data={'run_id': run_id})
+    r_execute_run = requests.post(f"{api_root}runs/{run_id}/execute/", data={"run_id": run_id})
     print(f"Run response: {r_execute_run.text}")
 
     # 7. Create a new Step
     step_inventory = {"id": 1, "command": "echo test"}
-    r_create_step2 = requests.post(f"{api_root}steps/", data={'stage_id': stage_id},
-                                   files={"file": yaml.dump(STEP_REUSABLE),
-                                          "inventory_file": yaml.dump(step_inventory)})
-    step_id2 = r_create_step2.json()['id']
+    r_create_step2 = requests.post(
+        f"{api_root}steps/",
+        data={"stage_id": stage_id},
+        files={"file": yaml.dump(STEP_REUSABLE), "inventory_file": yaml.dump(step_inventory)},
+    )
+    step_id2 = r_create_step2.json()["id"]
     print(f"Second step id: {step_id2}")
 
     # 8. Execute the new Step (First, get Stage execution's id)
-    stage_execution_id = requests.get(f"{api_root}runs/{run_id}/report/")\
-        .json()["detail"]["plan_executions"][0]["stage_executions"][0]["id"]
-    r_execute_step = requests.post(f"{api_root}steps/{step_id2}/execute/",
-                                   data={'stage_execution_id': stage_execution_id})
+    stage_execution_id = requests.get(f"{api_root}runs/{run_id}/report/").json()["detail"]["plan_executions"][0][
+        "stage_executions"
+    ][0]["id"]
+    r_execute_step = requests.post(
+        f"{api_root}steps/{step_id2}/execute/", data={"stage_execution_id": stage_execution_id}
+    )
     print(f"Second Step response: {r_execute_step.text}")
 
     # 9. Get Run report
